172d003b1b68ace4194caf24a8bf280507c18404
Dreaser <dams.payne@gmail.com>
2014-05-20 14:02:28 +0200

    worked on the rooting

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-var phonecatApp = angular.module('togglePlayApp', [
-  'togglePlayControllers',
+'use strict';
+
+var togglePlayApp = angular.module('togglePlayApp', [
+  'ngRoute',
+  'togglePlayControllers'
 ]);
+togglePlayApp.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/la', {
+        templateUrl: 'views/partials/partial1.html',
+        controller: 'togglePlayCtrl'
+      }).
+      when('/home', {
+        templateUrl: 'views/home.html'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      });
+    $locationProvider.html5Mode(true);
+  }]);
--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -1,10 +1,14 @@
+'use strict';
+
 var togglePlayControllers = angular.module('togglePlayControllers', []);
 
 togglePlayControllers.controller('togglePlayCtrl', ['$scope',
   function ($scope) {
-	var mongoose = require('mongoose');  
+	console.log("ici");
+	$scope.appname="salut";
+	/*var mongoose = require('mongoose');  
 	mongoose.connect('mongodb://localhost/test', function(err) {
 		if (err) { throw err; }
-	});
+	});*/
   }
 ]);
