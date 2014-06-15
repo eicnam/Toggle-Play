c8b1b1064cfc675c505c7c5de5b0daecc53c02ef
Bayk <admiquel94@gmail.com>
2014-06-15 17:28:47 +0200

    Improvement on App Controller #48
    1. Changing name to specify what it concerns.
    2. Implementing all the defined actions in server API

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -3,18 +3,45 @@
 var togglePlayApp = angular.module('togglePlayApp', [
   'ngRoute',
   'togglePlayControllers'
-]);
-togglePlayApp.config(['$routeProvider',
-  function($routeProvider) {
+  ]);
+togglePlayApp.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/', {
-        templateUrl: 'views/partials/partial1.html',
-        controller: 'togglePlayCtrl'
+      when('/Apps/Add', {
+        templateUrl: 'views/partials/application_add.html',
+        controller : 'AppAddCtrl'
       }).
-      when('/home', {
+      when('/Apps/Update', {
+        templateUrl: 'views/partials/application_update.html',
+        controller : 'AppUpdateCtrl'
+      }).
+      when('/Apps/Update/:appid', {
+        templateUrl: 'views/partials/application_update.html',
+        controller : 'AppUpdateCtrl'
+      }).
+      when('/Apps/Delete/:appid', {
+        templateUrl: 'views/partials/application_delete.html',
+        controller : 'AppDeleteCtrl'
+      }).
+      when('/Apps/Delete', {
+        templateUrl: 'views/partials/application_delete.html',
+        controller : 'AppDeleteCtrl'
+      }).
+      when('/Apps', {
+        templateUrl: 'views/partials/apps.html',
+        controller : 'AppsCtrl'
+      }).
+      when('/Apps/:appName', {
+        templateUrl: 'views/partials/application_details.html',
+        controller : 'AppDetailsCtrl'
+      }).
+      when('/', {
         templateUrl: 'views/home.html'
       }).
       otherwise({
         redirectTo: '/'
       });
-  }]);
+
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+}]);
