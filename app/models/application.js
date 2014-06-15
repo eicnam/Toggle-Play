88c5c189b8999fcd4331f5719f418964e8adba4c
Bayk <admiquel94@gmail.com>
2014-06-15 17:18:23 +0200

    First improvement for App's model #7
    New fields :
    - Date of publication
    - Date of last Update
    
    AND
    
    Validation functions but the integration of these functions makes
    troubles for adding apps ... TO FIX

--- app/models/application.js
+++ app/models/application.js
@@ -2,9 +2,23 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ApplicationSchema   = new Schema({
-	name: String,
-	author: String,
-	description : String
+	name: { type: String, required: true/*, validate: [nameValidator, 'Invalid name']*/ }
+	, author: String
+	, description : String
+	, publicationDate : { type: Date, default: Date.now}
+	, lastUpdateDate : { type: Date, default: Date.now}
 });
+
+function nameValidator(name){
+	return name.length >= 4
+		&& name.length >= 20
+		&& new RegExp("/[^a-zA-Z0-9 &\s]/gi").test(name);
+}
+
+function authorValidator(author){
+	return author.length >= 5
+		&& author.length >= 30
+		&& new RegExp("/[^a-zA-Z \s]/gi").test(author);
+}
 
 module.exports = mongoose.model('Application', ApplicationSchema);
